bcc092a4fcc99a9151e67472499944b62ad388ff
Marija Marin <marijamarin2508@gmail.com>
2020-07-20 15:56:45 +0200

    kvad.js

---
+++ kvad.js
@@ -0,0 +1,19 @@
+{
+	let a = 10,
+		b = 5;
+	if (a >= 0 && b >= 0) {
+		let O, P;
+		O = 2 * a + 2 * b;
+		P = a * b;
+		console.log(O, P);
+	}
+}
+{
+	let a = 3;
+	if (a >= 0) {
+		let O, P;
+		O = 4 * a;
+		P = a * a;
+		console.log(O, P);
+	}
+}
